d967e2c419c67110a934121cff769b1db2cdb6f1
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-05-21 21:01:47 -0300

    Update VMTestsRunner.js

--- tests/VMTestsRunner.js
+++ tests/VMTestsRunner.js
@@ -1,10 +1,10 @@
 const async = require('async')
 const VM = require('../')
-const Account = require('ethereumjs-account')
+const Account = require('puffscoinjs-account')
 const testUtil = require('./util')
 const Trie = require('merkle-patricia-tree/secure')
-const ethUtil = require('ethereumjs-util')
-const BN = ethUtil.BN
+const puffsUtil = require('puffscoinjs-util')
+const BN = puffsUtil.BN
 
 module.exports = function runVMTest (options, testData, t, cb) {
   let state = new Trie()
