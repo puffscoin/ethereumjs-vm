4ca7bb45d2fd7a1206001a4e5eeb1abf05457066
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-05-21 21:06:45 -0300

    Update util.js

--- tests/util.js
+++ tests/util.js
@@ -1,10 +1,10 @@
 const async = require('async')
-const utils = require('ethereumjs-util')
+const utils = require('puffscoinjs-util')
 const BN = utils.BN
 const rlp = utils.rlp
-const Account = require('ethereumjs-account')
-const Transaction = require('ethereumjs-tx')
-const Block = require('ethereumjs-block')
+const Account = require('puffscoinjs-account')
+const Transaction = require('puffscoinjs-tx')
+const Block = require('puffscoinjs-block')
 
 exports.dumpState = function (state, cb) {
   function readAccounts (state) {
